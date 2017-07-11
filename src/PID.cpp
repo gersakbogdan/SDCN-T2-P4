232661502bc4ec474b8fbe36338946d1558bbe2f
Bogdan Gersak <bogdan@gersak.ro>
2017-07-11 21:54:27 +0200

    Implement PID Controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,23 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    Kp_ = Kp;
+    Ki_ = Ki;
+    Kd_ = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    d_error_  = cte - p_error_;
+    p_error_  = cte;
+    i_error_ += cte;
 }
 
 double PID::TotalError() {
+    return (-Kp_ * p_error_) + (-Kd_ * d_error_) + (-Ki_ * i_error_);
 }
 
